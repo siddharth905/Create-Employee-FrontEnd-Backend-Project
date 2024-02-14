56c938268539be8d8866a7ee7c8968eb24c428c0
siddharth905 <siddharthkalamkar905@gmail.com>
2024-02-14 20:24:46 +0530

    Backed Run sucessfully

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const dbConnect = require("./config/database");
-//const userRoutes = require("./routes/user");
+const userRoutes = require("./routes/user");
 const app = express();
-//var cors = require("cors");
+var cors = require("cors");
 const PORT = process.env.PORT || 4000;
 
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
-// Middleware
+ app.use(
+   cors({
+      origin: "*",
+    })
+  );
+ //Middleware
 app.use(express.json());
 
-//app.use("/api/v1", userRoutes);
+app.use("/api/v1", userRoutes);
 
 // CORS Configuration
 app.listen(PORT, () => {
