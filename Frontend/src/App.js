c8305af896a1327b0ad972fe0d23c44cddbb3cac
siddharth905 <siddharthkalamkar905@gmail.com>
2024-02-14 18:27:46 +0530

    Fronted:- EmployeeForm Components Created And Important things Added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-//import HomePage from "./pages/HomePage";
-//import CreateEmployeePage from "./pages/CreateEmployeePage";
-
+import HomePage from "./pages/HomePage";
+//import createEmployeePage from "../pages/createEmployeePage";
+import CreateEmployeePage from "./pages/createEmployeePage";
 function App() {
   return (
     <div>
